14c4f69fec29dafeb6324b22450733dc9fc4ad63
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-09 14:09:57 +0300

    Database configuration

---
+++ database.py
@@ -0,0 +1,49 @@
+import motor.motor_asyncio
+from fastapi import FastAPI, Body
+from typing_extensions import Annotated
+from pydantic.functional_validators import BeforeValidator
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+from bson import ObjectId
+
+app = FastAPI()
+
+MONGODB_URL="mongodb://127.0.0.1?retryWrites=true&w=majority"
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
+db = client.fastapi
+product_collection = db.get_collection('product')
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class Product(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id', default=None)
+    name: str 
+    price: int
+    discount: int
+    discount_price: float
+    # model_config = ConfigDict(
+    #     populate_by_name=True,
+    #     arbitrary_types_allowed=True
+    # )
+
+def product_helper(product) -> dict:
+    return {
+        "id": str(product["_id"]),
+        "name": product["name"],
+        "price": product["price"],
+        "discount": product["discount"],
+        "discount_price": product["discount_price"],
+    }
+
+@app.post('/product')
+async def add_product(product: Product):
+    print("Call API '/product'")
+    new_product = await product_collection.insert_one(
+        product.model_dump(by_alias=True, exclude=['id'])
+    )
+    created_product = await product_collection.find_one(
+        {'_id': new_product.inserted_id}
+    )
+    return product_helper(created_product)
