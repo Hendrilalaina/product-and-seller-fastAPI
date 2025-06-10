1cf9c68606ae2041788e9c2f8322c49c647ad5d4
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-10 09:53:57 +0300

    Add seller_collection

--- product/database.py
+++ product/database.py
@@ -4,3 +4,4 @@ MONGODB_URL="mongodb://127.0.0.1?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client.get_database('fastapi')
 product_collection = db.get_collection('product')
+seller_collection = db.get_collection('seller')
