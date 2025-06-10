a52f17ee486b0d3202e7b3e7a01daa4419b644f6
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-10 08:47:51 +0300

    Add UpdateProduct model

--- product/schema.py
+++ product/schema.py
@@ -6,12 +6,24 @@ from bson import ObjectId
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
+
 class Product(BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id', default=None)
     name: str 
     price: int
     discount: int
     discount_price: float
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str}
+    )
+
+class UpdateProduct(BaseModel):
+    name: Optional[str]
+    price: Optional[int]
+    discount: Optional[int]
+    discount_price: Optional[float]
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True,
