4a1885892eaef922f3001e56c09d759d3e182f1e
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-10 09:09:07 +0300

    Add update_product

--- product/main.py
+++ product/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from typing import List
 from bson import ObjectId
 from bson.errors import InvalidId
+from pymongo import ReturnDocument
 
 from schema import Product, UpdateProduct
 from database import product_collection
@@ -52,3 +53,30 @@ async def get_product(id: str):
         return product
     
     raise HTTPException(status_code=404, detail=f"Product {id} not found")
+
+@app.put('/product/{id}',
+    response_description="Update a product",
+    response_model=Product,
+    response_model_by_alias=False    
+)
+async def update_product(id: str, product: UpdateProduct):
+    product = {
+        k: v for k, v in product.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(product) >= 1:
+        update_product = await product_collection.find_one_and_update(
+            {"_id": parse_object_id(id)},
+            {"$set": product},
+            return_document=ReturnDocument.AFTER
+        )
+        if update_product is not None:
+            return update_product
+        else:
+            raise HTTPException(status_code=404, detail=f"Product {id} is not found")
+
+    # len(product) == 0 but id exists
+    if (exist_product := await product_collection.find_one({'_id': parse_object_id(id)})) is not None:
+        return exist_product
+    
+    raise HTTPException(status_code=404, detail=f"Product {id} is not found")
