d4c02c6fa8fdd36edf1accfff640e3b2fc2807e7
Hendrilalaina <hendrilalaina@gmail.com>
2025-07-01 15:30:53 +0300

    Adding title, description and tags

--- product/main.py
+++ product/main.py
@@ -6,9 +6,15 @@ from auth import generate_token, get_current_user, hash_password, verify_passwor
 from schema import Product, UpdateProduct, Seller, Token, UserRequest
 from database import product_collection, seller_collection
 
-app = FastAPI()
+app = FastAPI(
+    title="Products API",
+    description="Manage products in website",
+    contact={
+        "email": "hendrilalaina@gmail.com",
+    },)
 
 @app.post('/seller',
+    tags=["Authentication"],
     description="Register a seller",
     response_model=Seller,
     response_model_by_alias=False,
@@ -26,6 +32,7 @@ async def add_seller(seller: Seller):
     return created_seller
 
 @app.post('/login',
+    tags=["Authentication"],
     description="Login the seller",
     response_model=Token,
     response_model_by_alias=False,)
@@ -42,13 +49,16 @@ async def login_seller(user_request: UserRequest):
 
     return Token(access_token=access_token, token_type="bearer")
 
-@app.get('/me', 
+@app.get('/me',
+    tags=["Authentication"],
+    description="The login user",
     response_model=Seller,
     response_model_exclude={"password"},)
 async def read_current_user(current_user: dict = Depends(get_current_user)):
     return current_user
 
 @app.get('/product',
+    tags=["Product"],
     description="Get all products",
     response_model=List[Product],
     response_model_by_alias=False,)
@@ -56,6 +66,7 @@ async def get_products():
     return await product_collection.find().to_list()
 
 @app.get('/product/{id}',
+    tags=["Product"],
     description="Get a product by id",
     response_model=Product,
     response_model_by_alias=False,)
@@ -68,6 +79,7 @@ async def get_product(id: str):
     raise HTTPException(status_code=404, detail=f"Product {id} not found")
 
 @app.post('/product',
+    tags=["Product"],
     description="Add new product",
     response_model=Product,
     status_code=status.HTTP_201_CREATED,
@@ -83,6 +95,7 @@ async def add_product(product: Product, seller: dict = Depends(get_current_user)
     return created_product
 
 @app.put('/product/{id}',
+    tags=["Product"],
     description="Update a product",
     response_model=Product,
     response_model_by_alias=False,)
@@ -109,6 +122,7 @@ async def update_product(id: str, product: UpdateProduct, seller: dict = Depends
     raise HTTPException(status_code=404, detail=f"Product {id} is not found")
 
 @app.delete('/product/{id}',
+    tags=["Product"],
     description="Remove a product",)
 async def delete_product(id: str, seller: dict = Depends(get_current_user)):
     remove_product = await product_collection.find_one_and_delete(
