aba6edf33c1c28149898d21b78b87565b9046340
Hendrilalaina <hendrilalaina@gmail.com>
2025-07-01 13:40:01 +0300

    Adding delete product

--- product/main.py
+++ product/main.py
@@ -8,72 +8,8 @@ from database import product_collection, seller_collection
 
 app = FastAPI()
 
-@app.post('/product',
-    response_description="Add new product",
-    response_model=Product,
-    status_code=status.HTTP_201_CREATED,
-    response_model_by_alias=False,)
-async def add_product(product: Product, seller: dict = Depends(get_current_user)):
-    product.seller_id = str(seller['_id'])
-    new_product = await product_collection.insert_one(
-        product.model_dump(by_alias=True, exclude=['id']))
-
-    created_product = await product_collection.find_one(
-        {'_id': new_product.inserted_id})
-
-    return created_product
-
-@app.get('/product',
-    response_description="Get all products",
-    response_model=List[Product],
-    response_model_by_alias=False,)
-async def get_products():
-    return await product_collection.find().to_list()
-
-@app.get('/product/{id}',
-    response_description="Get a product by id",
-    response_model=Product,
-    response_model_by_alias=False,)
-async def get_product(id: str):
-    if (
-        product := await product_collection.find_one({'_id': parse_object_id(id)})
-    ) is not None:
-        return product
-    
-    raise HTTPException(status_code=404, detail=f"Product {id} not found")
-
-@app.put('/product/{id}',
-    response_description="Update a product",
-    response_model=Product,
-    response_model_by_alias=False,)
-async def update_product(id: str, product: UpdateProduct, seller: dict = Depends(get_current_user)):
-    product = {
-        k: v for k, v in product.model_dump(by_alias=True).items() if v is not None
-    }
-    product['seller_id'] = seller['_id']
-
-    # if 'seller_id' in product.keys():
-        # await check_seller(product['seller_id'])
-    
-    if len(product) >= 1:
-        update_product = await product_collection.find_one_and_update(
-            {"_id": parse_object_id(id), "seller_id": product['seller_id']},
-            {"$set": product},
-            return_document=ReturnDocument.AFTER,)
-
-        if update_product is not None:
-            return update_product
-        else:
-            raise HTTPException(status_code=404, detail=f"Product {id} is not found")
-
-    # len(product) == 0 but id exists
-    if (exist_product := await product_collection.find_one({'_id': parse_object_id(id)})) is not None:
-        return exist_product
-    
-    raise HTTPException(status_code=404, detail=f"Product {id} is not found")
-
 @app.post('/seller',
-    response_description="Add a seller",
+    response_description="Register a seller",
     response_model=Seller,
     response_model_by_alias=False,
     status_code=status.HTTP_201_CREATED,)
@@ -111,3 +47,73 @@ async def login_seller(user_request: UserRequest):
     response_model_exclude={"password"},)
 async def read_current_user(current_user: dict = Depends(get_current_user)):
     return current_user
+
+@app.get('/product',
+    response_description="Get all products",
+    response_model=List[Product],
+    response_model_by_alias=False,)
+async def get_products():
+    return await product_collection.find().to_list()
+
+@app.get('/product/{id}',
+    response_description="Get a product by id",
+    response_model=Product,
+    response_model_by_alias=False,)
+async def get_product(id: str):
+    if (
+        product := await product_collection.find_one({'_id': parse_object_id(id)})
+    ) is not None:
+        return product
+    
+    raise HTTPException(status_code=404, detail=f"Product {id} not found")
+
+@app.post('/product',
+    response_description="Add new product",
+    response_model=Product,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,)
+async def add_product(product: Product, seller: dict = Depends(get_current_user)):
+    product.seller_id = str(seller['_id'])
+    new_product = await product_collection.insert_one(
+        product.model_dump(by_alias=True, exclude=['id']))
+
+    created_product = await product_collection.find_one(
+        {'_id': new_product.inserted_id})
+
+    return created_product
+
+@app.put('/product/{id}',
+    response_description="Update a product",
+    response_model=Product,
+    response_model_by_alias=False,)
+async def update_product(id: str, product: UpdateProduct, seller: dict = Depends(get_current_user)):
+    product = {
+        k: v for k, v in product.model_dump(by_alias=True).items() if v is not None
+    }
+    
+    if len(product) >= 1:
+        update_product = await product_collection.find_one_and_update(
+            {"_id": parse_object_id(id), "seller_id": seller['_id']},
+            {"$set": product},
+            return_document=ReturnDocument.AFTER,)
+
+        if update_product is not None:
+            return update_product
+        else:
+            raise HTTPException(status_code=404, detail=f"Product {id} is not found")
+
+    # len(product) == 0 but id exists
+    if (exist_product := await product_collection.find_one({'_id': parse_object_id(id)})) is not None:
+        return exist_product
+    
+    raise HTTPException(status_code=404, detail=f"Product {id} is not found")
+
+@app.delete('/product/{id}',
+    response_description="Remove a product",)
+async def delete_product(id: str, seller: dict = Depends(get_current_user)):
+    remove_product = await product_collection.find_one_and_delete(
+        {"_id": parse_object_id(id), "seller_id": seller['_id']}
+    )
+    if not remove_product:
+        raise HTTPException(status_code=404, detail=f"Product {id} is not found!")
+    return f"Product {id} is deleted!"
